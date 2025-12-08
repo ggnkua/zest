3db3733d27bfac658a63d63459f292d12e5510d4
ggn <ggn@atari.org>
2025-12-08 16:12:20 +0200

    Added different random number generator for jukebox mode

--- linux/infomsg.c
+++ linux/infomsg.c
@@ -22,6 +22,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/time.h>
+#include <time.h>
 #include <dirent.h>
 
 #include "osd.h"
@@ -177,6 +178,38 @@ void vol_up(void) {
   }
 }
 
+// *Really* minimal PCG32 code / (c) 2014 M.E. O'Neill / pcg-random.org
+// Licensed under Apache License 2.0 (NO WARRANTY, etc. see website)
+
+typedef struct { uint64_t state;  uint64_t inc; } pcg32_random_t;
+
+uint32_t pcg32_random_r(pcg32_random_t* rng)
+{
+  uint64_t oldstate = rng->state;
+  // Advance internal state
+  rng->state = oldstate * 6364136223846793005ULL + (rng->inc|1);
+  // Calculate output function (XSH RR), uses old state for max ILP
+  uint32_t xorshifted = ((oldstate >> 18u) ^ oldstate) >> 27u;
+  uint32_t rot = oldstate >> 59u;
+  return (xorshifted >> rot) | (xorshifted << ((-rot) & 31));
+}
+
+void pcg32_srandom_r(pcg32_random_t* rng, uint64_t initstate, uint64_t initseq)
+{
+  rng->state = 0U;
+  rng->inc = (initseq << 1u) | 1u;
+  pcg32_random_r(rng);
+  rng->state += initstate;
+  pcg32_random_r(rng);
+}
+
+pcg32_random_t random_for_disk_selection;
+
+void init_pcg(void)
+{
+  pcg32_srandom_r(&random_for_disk_selection, time(NULL), (intptr_t)&random_for_disk_selection);
+}
+
 void * thread_jukebox(void * arg) {
   uint64_t jukebox_timeout = 0;
   while (thr_end == 0) {
@@ -194,10 +227,13 @@ void * thread_jukebox(void * arg) {
           jukebox_timeout = time + 1000000;
         } else {
           // Select random image
-          srand(time);
+          if (!random_for_disk_selection.state)
+          {
+            init_pcg();
+          }
           int selected_image;
           do {
-            selected_image = rand() % n;
+            selected_image = pcg32_random_r(&random_for_disk_selection) % n;
           } while (namelist[selected_image]->d_type==DT_DIR); // Avoid directories
           // Construct image filename
           char *selected_item = namelist[selected_image]->d_name;
