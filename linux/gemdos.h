ffe0482b0d490e8e4ca9932d1c809d366f4e319a
François Galea <fgalea@free.fr>
2026-01-01 10:21:39 +0100

    Bumped copyright years.

--- linux/gemdos.h
+++ linux/gemdos.h
@@ -1,7 +1,7 @@
 /*
  * gemdos.h - GEMDOS drive implementation
  *
- * Copyright (c) 2025 Francois Galea <fgalea at free.fr>
+ * Copyright (c) 2025-2026 Francois Galea <fgalea at free.fr>
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
